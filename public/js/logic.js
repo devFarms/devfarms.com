33cd511e21efb56252682ac1ba18591738fe03fe
Alex Green <alex@devfarms.com>
2018-02-02 10:22:48 -0800

    Buttons and animation added.

---
+++ public/js/logic.js
@@ -0,0 +1,29 @@
+function secondSection(){
+    $("html,body").animate({
+        scrollTop: $("#second").offset().top},
+        "slow");
+};
+
+function thirdSection(){
+    $("html,body").animate({
+        scrollTop: $("#third").offset().top},
+        "slow");
+};
+
+function fourthSection(){
+    $("html,body").animate({
+        scrollTop: $("#fourth").offset().top},
+        "slow");
+};
+
+function fifthSection(){
+    $("html,body").animate({
+        scrollTop: $("#fifth").offset().top},
+        "slow");
+};
+
+function firstSection(){
+    $("html,body").animate({
+        scrollTop: $("#first").offset().top},
+        "slow");
+};
