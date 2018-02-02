51ce6f3d1ca30cc9cfa673bafdbeaa2a4ccb3141
Alex Green <alex@devfarms.com>
2018-02-01 21:34:30 -0800

    Corrected PORT 3000 problem.

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ var app = express();
 app.use(express.static("public"));
 
 // Set the app to listen on port 3000
-app.listen(3000, function() {
-  console.log("App running on port 3000!");
+app.set("port", (process.env.PORT || 3000));
+
+app.listen(app.get("port"), function(){
+    console.log("Server started on port " +app.get("port"))
 });
